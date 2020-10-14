ae218471ecd392ef0dddcee5a6c665d182ce0fdc
duc.cao <caominhduc1999@gmail.com>
2020-10-14 21:53:35 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>hello</h1>
+        <button class="btn btn-danger">hello</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
